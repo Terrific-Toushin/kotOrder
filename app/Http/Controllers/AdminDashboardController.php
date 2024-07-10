335e5593fb898309f9cd14b3320c00f1f2baeae0
Md Abdullah Tousin <mdabdullahtoushin@gmail.com>
2024-07-10 16:54:14 +0600

    dashboard index change

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -19,24 +19,23 @@ class AdminDashboardController extends Controller
 {
     public function index(){
 
-        $users = UserPrivileges::selectRaw('count(user_id) as total, SUM(CASE WHEN user_type = "SU" THEN 1 ELSE 0 END) as supervisors_count,
+        $users = UserPrivileges::selectRaw('count(user_id) as total, SUM(CASE WHEN user_type = "operator" THEN 1 ELSE 0 END) as operator_operator,
                             SUM(CASE WHEN user_type = "AU" THEN 1 ELSE 0 END) as admins_count,
-                            SUM(CASE WHEN user_type = "MU" THEN 1 ELSE 0 END) as manager_count,
-                            SUM(CASE WHEN user_type = "EU" THEN 1 ELSE 0 END) as executive_count')
+                            SUM(CASE WHEN user_type = "kitchen" THEN 1 ELSE 0 END) as kitchen_count')->where('email','!=', 'toushin.java@gmail.com')
             ->first();
-        $panding_kots_count = DB::table('order_kot')->where('status', '=', '1')->where('cancel', '=', 'N')->count();
+        $panding_kots_count = DB::table('order_kot')->where('cancel', '=', 'N')->where(function ($q) {$q->where('status', '=', '1')->orWhere('status', '=', '4');})->count();
         $kitchen_complete_kot_count = DB::table('order_kot')->where('status', '=', '3')->where('cancel', '=', 'N')->count();
         $total_kots_count = DB::table('order_kot')->where('cancel', '=', 'N')->count();
         $cash_print_count = DB::table('order_kot')->where('status', '=', '2')->where('cancel', '=', 'N')->count();
         $kot_cancel_count = DB::table('order_kot')->where('cancel', '=', 'Y')->count();
+        $outlets_count=DB::table('order_kot')->selectRaw('outlet, SUM(CASE WHEN cancel = "N" AND (status = "1" OR status = "4") THEN 1 ELSE 0 END) as panding_kots_count,
+                                    SUM(CASE WHEN cancel = "N" AND status = "3" THEN 1 ELSE 0 END) as kitchen_complete_kot_count,
+                                    SUM(CASE WHEN cancel = "N" THEN 1 ELSE 0 END) as total_kots_count,
+                                    SUM(CASE WHEN cancel = "N" AND status = "2" THEN 1 ELSE 0 END) as cash_print_count,
+                                    SUM(CASE WHEN cancel = "Y" THEN 1 ELSE 0 END) as kot_cancel_count')
+            ->first();
 
-        $tblRestName_data = DB::connection('sqlsrv')->table('tblRestName')->orderBy('ResName')->get();
-        $outlets_count=[];
-        foreach ($tblRestName_data as $key => $outlets) {
-            # code...
-        }
-
-        return view('admin.dashboard.dashboard')->with(compact('users','panding_kots_count','kitchen_complete_kot_count','total_kots_count','cash_print_count'));
+        return view('admin.dashboard.dashboard')->with(compact('users','panding_kots_count','kitchen_complete_kot_count','total_kots_count','cash_print_count','outlets_count'));
     }
 
     public function redirects(){
